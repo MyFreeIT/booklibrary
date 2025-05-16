95689a439da3583b05d7f0cd8e49227a2461c8eb
MyFreeIT <MyFreeIT@outlook.com>
2025-05-16 04:51:13 +0300

    Added Person.java to the package models.

---
+++ src/main/java/com/github/myfreeit/booklibrary/models/Person.java
@@ -0,0 +1,56 @@
+package com.github.myfreeit.booklibrary.models;
+
+/*
+ * Copyright (c) 2025, Denis Odesskiy. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of Denis Odesskiy
+ * ("Confidential Information"). You shall not disclose such Confidential Information
+ * and shall use it only in accordance with the terms of the license agreement you
+ * entered into with Denis Odesskiy.
+ */
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class Person {
+  private int id;
+
+  @NotEmpty(message = "{name.empty}")
+  @Size(min = 2, max = 100, message = "{name.size}")
+  private String fullName;
+
+  @Min(value = 1900, message = "{age.min}")
+  private int yearOfBirth;
+
+  public Person() {}
+
+  public Person(String fullName, int yearOfBirth) {
+    this.fullName = fullName;
+    this.yearOfBirth = yearOfBirth;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getFullName() {
+    return fullName;
+  }
+
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
+
+  public int getYearOfBirth() {
+    return yearOfBirth;
+  }
+
+  public void setYearOfBirth(int yearOfBirth) {
+    this.yearOfBirth = yearOfBirth;
+  }
+}
