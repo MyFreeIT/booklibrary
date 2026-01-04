bf1963657d306fef9d50bc0b4ea1dc841ffc589b
MyFreeIT <MyFreeIT@outlook.com>
2026-01-05 01:13:56 +0200

    feat(controller): add book search endpoints - Added GET /books/search to show search page - Added POST /books/search to process query - Integrated BooksService.searchByTitle with model

--- src/main/java/com/github/myfreeit/booklibrary/controllers/BooksController.java
+++ src/main/java/com/github/myfreeit/booklibrary/controllers/BooksController.java
@@ -115,4 +115,15 @@ public class BooksController {
     booksService.assign(id, selectedPerson);
     return "redirect:/books/" + id;
   }
+
+  @GetMapping("/search")
+  public String searchPage() {
+    return "books/search";
+  }
+
+  @PostMapping("/search")
+  public String makeSearch(Model model, @RequestParam("query") String query) {
+    model.addAttribute("books", booksService.searchByTitle(query));
+    return "books/search";
+  }
 }
